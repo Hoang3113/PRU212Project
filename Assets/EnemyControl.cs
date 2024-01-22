505247964b0e85eb8a475f0356d07255193eb793
Minh Nguyên <108600635+Nguyennm080503@users.noreply.github.com>
2024-01-22 21:16:08 +0700

    script enemy movement

---
+++ Assets/EnemyControl.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControl : MonoBehaviour
+{
+    public GameObject pointA;
+    public GameObject pointB;
+    private Rigidbody2D rb;
+    private Transform currentpoint;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        currentpoint = pointA.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 point = currentpoint.position - transform.position;
+        if (currentpoint == pointA.transform)
+        {
+            rb.velocity = new Vector2(-speed, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector2(speed, 0);
+        }
+
+        if (Vector2.Distance(transform.position, currentpoint.position) < 0.5f && currentpoint == pointA.transform)
+        {
+            flip();
+            currentpoint = pointB.transform;
+        }
+        if (Vector2.Distance(transform.position, currentpoint.position) < 0.5f && currentpoint == pointB.transform)
+        {
+            flip();
+            currentpoint = pointA.transform;
+        }
+    }
+
+    private void flip()
+    {
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(pointA.transform.position, 0.5f);
+        Gizmos.DrawWireSphere(pointB.transform.position, 0.5f);
+        Gizmos.DrawLine(pointA.transform.position, pointB.transform.position);
+    }
+}
