a315e7709ae9bd8e750e4cbe56697dd84ab9bdf2
tinhle2607 <lecongtinh223@gmail.com>
2024-02-27 08:03:15 +0700

    a

--- Assets/CoinsCollector.cs
+++ Assets/CoinsCollector.cs
@@ -1,26 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class CoinsCollector : MonoBehaviour
 {
-    
     public int coins = 0;
+    public Text coinsText;
 
-    [SerializeField] public Text coinsText;
+    private void Start()
+    {
+       
+        coins = PlayerPrefs.GetInt("Coins", 0);
+        UpdateCoinsText();
+    }
 
-    
+    private void UpdateCoinsText()
+    {
+        coinsText.text = "Coins: " + coins.ToString();
+    }
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Coins"))
         {
-          
             Destroy(collision.gameObject);
             coins++;
-            coinsText.text = "Coins: " + coins;
+            UpdateCoinsText();
         }
     }
-}
 
+    private void OnDestroy()
+    {
+        // Lưu số coin vào PlayerPrefs trước khi chuyển scene hoặc kết thúc game
+        PlayerPrefs.SetInt("Coins", coins);
+        PlayerPrefs.Save(); 
+    }
+}
