6630e7ba5e5aff84f6fc7c271567ccefee0ded18
Nhat Truong <nhattruong3314@gmail.com>
2024-03-07 12:12:59 +0700

    Truongg

--- Assets/Scripts/Boss/Goblin_Attack.cs
+++ Assets/Scripts/Boss/Goblin_Attack.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.TextCore.Text;
 
 public class Goblin_Attack : MonoBehaviour
 {
@@ -11,6 +10,12 @@ public class Goblin_Attack : MonoBehaviour
     public Vector3 attackOffset;
     public float attackRange;
     public LayerMask attackMask;
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    public int spell_conddition;
+    public Animator animator;
+
+    private int mana = 0;
 
     public void Attack()
     {
@@ -21,7 +26,7 @@ public class Goblin_Attack : MonoBehaviour
         Collider2D colInfo = Physics2D.OverlapCircle(pos, attackRange, attackMask);
         if (colInfo != null)
         {
-            colInfo.GetComponent<Player_Heath>().TakeDamage(attackDamage);
+            //colInfo.GetComponent<PlayerHealth>().TakeDamage(attackDamage);
         }
     }
     void OnDrawGizmosSelected()
@@ -31,5 +36,20 @@ public class Goblin_Attack : MonoBehaviour
         pos += transform.up * attackOffset.y;
 
         Gizmos.DrawWireSphere(pos, attackRange);
+    }
+    void Shoot()
+    {
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
+
+    private void Update()
+    {
+        mana++;
+        if (mana == spell_conddition)
+        {
+            animator.SetTrigger("Spell");
+            Shoot();
+            mana = 0;
+        }
     }
 }
